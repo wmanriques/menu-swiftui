de6eafadedea6ce68e79921c8ccbd6e92a33fb62
Manrique Salazar, Williams J <williams.manrique@one.verizon.com>
2020-04-17 10:48:17 -0500

    step one

---
+++ menu/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  menu
+//
+//  Created by wsk on 4/17/20.
+//  Copyright © 2020 wsk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var isOpen: Bool = false
+    
+    var body: some View {
+        ZStack {
+            NavigationView{
+                Text("content")
+                .navigationBarItems(leading: burger)
+            }
+            menu
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension ContentView {
+    
+    var burger: some View {
+        Button(action: {
+            self.isOpen.toggle()
+        }) {
+            Image(systemName: "line.horizontal.3")
+        }
+    }
+    
+    var menu: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.white)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("menu")
+                Spacer()
+            }
+        }
+        .offset(x: isOpen ? 0 : -UIScreen.main.bounds.width)
+        .animation(.default)
+    }
+}
