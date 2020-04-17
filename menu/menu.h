6eb91fcb79ecce070d4ca4add90b72f214dc403b
Manrique Salazar, Williams J <williams.manrique@one.verizon.com>
2020-04-17 09:43:31 -0500

    Initial Commit

---
+++ menu/menu.h
@@ -0,0 +1,19 @@
+//
+//  menu.h
+//  menu
+//
+//  Created by wsk on 4/17/20.
+//  Copyright © 2020 wsk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for menu.
+FOUNDATION_EXPORT double menuVersionNumber;
+
+//! Project version string for menu.
+FOUNDATION_EXPORT const unsigned char menuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <menu/PublicHeader.h>
+
+
